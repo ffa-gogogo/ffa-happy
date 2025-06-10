9cd46f67908b84eee0d64bccdff83ae7eac004c6
郑雨飞 <1628012354@qq.com>
2025-06-10 16:29:19 +0800

    feat(all):修改中奖逻辑

--- src/containers/index/index.tsx
+++ src/containers/index/index.tsx
@@ -1,86 +1,273 @@
 import { ScratchCard } from "scratch-card-react";
-import Image from "./images/birthday/birthday.jpg";
+import Image from "./images/birthday/birthday-cover.jpg";
 import "./index.less";
 
 export default () => {
+  const arr = [""];
+  const getStr = () => {
+    const randomIndex = Math.floor(Math.random() * arr.length); // 生成随机索引
+    return arr[randomIndex];
+  };
+  const scratchBox = [
+    {
+      value: <div className="get-box">恭喜你！ 获得五等奖！！！</div>,
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: <div className="get-box">恭喜你！ 获得三等奖！！！</div>,
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: <div className="get-box">恭喜你！ 获得二等奖！！！</div>,
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: <div className="get-box">恭喜你！ 获得一等奖！！！</div>,
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: <div className="get-box">恭喜你！ 获得四等奖！！！</div>,
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+    {
+      value: (
+        <div className="noting-box">
+          很遗憾没得中奖哦~
+          <div className="tips">亲一口,才能继续哦</div>
+        </div>
+      ),
+      coverImg: Image,
+    },
+  ];
   return (
     <div className="index-home">
+      <div className="scratch-title">幸运呱呱乐！！</div>
       <div className="scratch-box">
-        <div className="scratch-item">
-          <ScratchCard width={300} height={200} coverImg={Image}>
-            <p>恭喜你 一等奖！</p>
-          </ScratchCard>
-        </div>
-        <div className="scratch-item">
-          <ScratchCard width={300} height={200} coverImg={Image}>
-            <p>恭喜你 一等奖！</p>
-          </ScratchCard>
-        </div>
-        <div className="scratch-item">
-          <ScratchCard width={300} height={200} coverImg={Image}>
-            <p>恭喜你 一等奖！</p>
-          </ScratchCard>
-        </div>
-        <div className="scratch-item">
-          <ScratchCard width={300} height={200} coverImg={Image}>
-            <p>恭喜你 一等奖！</p>
-          </ScratchCard>
-        </div>
-        <div className="scratch-item">
-          <ScratchCard width={300} height={200} coverImg={Image}>
-            <p>恭喜你 一等奖！</p>
-          </ScratchCard>
-        </div>
-        <div className="scratch-item">
-          <ScratchCard width={300} height={200} coverImg={Image}>
-            <p>恭喜你 一等奖！</p>
-          </ScratchCard>
-        </div>
-        <div className="scratch-item">
-          <ScratchCard width={300} height={200} coverImg={Image}>
-            <p>恭喜你 一等奖！</p>
-          </ScratchCard>
-        </div>
-        <div className="scratch-item">
-          <ScratchCard width={300} height={200} coverImg={Image}>
-            <p>恭喜你 一等奖！</p>
-          </ScratchCard>
-        </div>
-        <div className="scratch-item">
-          <ScratchCard width={300} height={200} coverImg={Image}>
-            <p>恭喜你 一等奖！</p>
-          </ScratchCard>
-        </div>
-        <div className="scratch-item">
-          <ScratchCard width={300} height={200} coverImg={Image}>
-            <p>恭喜你 一等奖！</p>
-          </ScratchCard>
-        </div>
-        <div className="scratch-item">
-          <ScratchCard width={300} height={200} coverImg={Image}>
-            <p>恭喜你 一等奖！</p>
-          </ScratchCard>
-        </div>
-        <div className="scratch-item">
-          <ScratchCard width={300} height={200} coverImg={Image}>
-            <p>恭喜你 一等奖！</p>
-          </ScratchCard>
-        </div>
-        <div className="scratch-item">
-          <ScratchCard width={300} height={200} coverImg={Image}>
-            <p>恭喜你 一等奖！</p>
-          </ScratchCard>
-        </div>
-        <div className="scratch-item">
-          <ScratchCard width={300} height={200} coverImg={Image}>
-            <p>恭喜你 一等奖！</p>
-          </ScratchCard>
-        </div>
-        <div className="scratch-item">
-          <ScratchCard width={300} height={200} coverImg={Image}>
-            <p>恭喜你 一等奖！</p>
-          </ScratchCard>
-        </div>
+        {scratchBox.map((item, index) => {
+          return (
+            <div className="scratch-item" key={index}>
+              <ScratchCard width={200} height={200} coverImg={item.coverImg}>
+                {item.value}
+              </ScratchCard>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
